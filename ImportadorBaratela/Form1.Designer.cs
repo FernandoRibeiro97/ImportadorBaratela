0d1c917af240d8d2f515546c688698a908c514e4
Fernando Ribeiro <fernando.ribeiro@imperiumsolucoes.com.br>
2026-01-16 11:19:24 -0300

    Adicionada a leitura do arquivos e um datagrid para visualizar os produtos do arquivo

--- ImportadorBaratela/Form1.Designer.cs
+++ ImportadorBaratela/Form1.Designer.cs
@@ -28,28 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.btnImportarArquivoCSV = new System.Windows.Forms.Button();
+            this.dgProduto = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProduto)).BeginInit();
             this.SuspendLayout();
             // 
             // btnImportarArquivoCSV
             // 
+            this.btnImportarArquivoCSV.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnImportarArquivoCSV.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.btnImportarArquivoCSV.Location = new System.Drawing.Point(12, 12);
             this.btnImportarArquivoCSV.Name = "btnImportarArquivoCSV";
-            this.btnImportarArquivoCSV.Size = new System.Drawing.Size(197, 70);
+            this.btnImportarArquivoCSV.Size = new System.Drawing.Size(197, 38);
             this.btnImportarArquivoCSV.TabIndex = 0;
             this.btnImportarArquivoCSV.Text = "Importar CSV";
-            this.btnImportarArquivoCSV.UseVisualStyleBackColor = true;
+            this.btnImportarArquivoCSV.UseVisualStyleBackColor = false;
             this.btnImportarArquivoCSV.Click += new System.EventHandler(this.btnImportarArquivoCSV_Click);
+            // 
+            // dgProduto
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.OldLace;
+            this.dgProduto.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgProduto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgProduto.Location = new System.Drawing.Point(12, 103);
+            this.dgProduto.Name = "dgProduto";
+            this.dgProduto.RowHeadersVisible = false;
+            this.dgProduto.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgProduto.Size = new System.Drawing.Size(1320, 614);
+            this.dgProduto.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1344, 729);
+            this.Controls.Add(this.dgProduto);
             this.Controls.Add(this.btnImportarArquivoCSV);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgProduto)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -57,6 +76,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnImportarArquivoCSV;
+        private System.Windows.Forms.DataGridView dgProduto;
     }
 }
 
--- ImportadorBaratela/Form1.cs
+++ ImportadorBaratela/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -23,18 +25,92 @@ namespace ImportadorBaratela
         }
         private void btnImportarArquivoCSV_Click(object sender, EventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Filter = "arquivos csv (*.csv)|*.csv";
-            ofd.ShowDialog();
+            string caminhoArquivo = string.Empty;
 
-            if (!string.IsNullOrEmpty(ofd.FileName))
+            using (OpenFileDialog ofd = new OpenFileDialog())
             {
+                ofd.Filter = "arquivos csv (*.csv)|*.csv";
 
+                if (ofd.ShowDialog() == DialogResult.OK && !string.IsNullOrEmpty(ofd.FileName))
+                    caminhoArquivo = ofd.FileName;
+                else
+                    MessageBox.Show("Nenhum arquivo selecionado");
+            }
+
+            if (File.Exists(caminhoArquivo))
+            {
+                LerArquivoCSV(File.ReadAllLines(caminhoArquivo));
             }
             else
             {
-                MessageBox.Show("Nenhum arquivo selecionado");
+                MessageBox.Show("Não foi possível ler o arquivo");
+                return;
             }
+        }
+
+        bool LerArquivoCSV(string[] linhas)
+        {
+            if (linhas.Length == 0)
+            {
+                MessageBox.Show("O arquivo está vazio");
+                return false;
+            }
+
+            DataTable dtProduto =  null;
+            int contLinha = 0;
+
+            foreach (string linha in linhas)
+            {
+                string[] campos = linha.Split(';');
+
+                if (contLinha == 0)
+                {
+                    dtProduto = MontarColunasDataTableCSV(campos);
+                }
+                else
+                {
+                    AdicionarLinhaDataTable(dtProduto, campos);
+                }
+
+                contLinha++;
+            }
+
+            if (dtProduto.Rows.Count > 0)
+            {
+                dgProduto.DataSource = dtProduto;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        DataTable MontarColunasDataTableCSV(string[] colunas)
+        {
+            DataTable dt = new DataTable();
+
+            if (colunas.Length > 0)
+            {
+                foreach (string coluna in colunas)
+                {
+                    dt.Columns.Add(coluna.Trim());
+                }
+            }
+            else
+            {
+                MessageBox.Show("Não foi possível ler as colunas do arquivo");
+            }
+
+            return dt;
+        }
+        bool AdicionarLinhaDataTable(DataTable dt, string[] campos)
+        {
+            if (campos.Length == dt.Columns.Count)
+            {
+                dt.Rows.Add(campos);
+                return true;
+            }
+
+            return false;
         }
     }
 }
